fd6dac84c98748c351f39b89aad52b1d5dc34c63
zhuliang <danlirencn@qq.com>
2023-11-20 22:37:52 +0800

    Shared element

--- app/src/main/java/com/github/pigcasso/samples/gridtopager/Config.kt
+++ app/src/main/java/com/github/pigcasso/samples/gridtopager/Config.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 
 object Config {
     var VIEWER_BACKGROUND_COLOR: Int = Color.BLACK
+    var DURATION_BG: Long = 150L
     var SWIPE_TOUCH_SLOP = 4f
     var DISMISS_FRACTION: Float = 0.12f
 }
